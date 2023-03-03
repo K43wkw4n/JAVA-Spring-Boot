4fac11983ae2e233f183ef7b5a2888b94c593852
Kaewkwan <sanung2002@gmail.com>
2023-03-03 15:07:23 +0700

    Controller and Service

---
+++ src/main/java/monsterservice/service/MonsterService.java
@@ -0,0 +1,41 @@
+package monsterservice.service;
+
+import monsterservice.model.Monster;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.List;
+
+@Service
+public class MonsterService {
+    public Monster postCreateMonsterService(Monster monster) {
+        return monster;
+    }
+
+    public List<Monster> getAllMonsterService() {
+        Monster monster = new Monster();
+        monster.setId(1);
+        return Collections.singletonList(monster);
+    }
+
+    public Monster getInformation(Integer id) {
+        Monster monster = new Monster();
+        monster.setId(id);
+        return monster;
+    }
+
+    public Monster updateMonsterByIdService(Monster monster) {
+        return monster;
+    }
+
+    public boolean deleteMonsterService(Integer id) {
+        return id > 0;
+    }
+
+    public Monster attackMonsterService(Integer id, Integer health) {
+        Monster monster = new Monster();
+        monster.setId(id);
+        monster.setHealth(health);
+        return monster;
+    }
+}
